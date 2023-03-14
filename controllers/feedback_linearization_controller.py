44ed33f90ef64d6cdb991dc72d6ac6163d717325
Andrzej Rafalski <anraf1001@gmail.com>
2023-03-14 22:55:27 +0100

    Add FLC

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -12,4 +12,19 @@ class FeedbackLinearizationController(Controller):
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-        return NotImplementedError()
+
+        q1, q2, q1_dot, q2_dot = x
+
+        q = np.array([[q1], [q2]])
+        q_dot = np.array([[q1_dot], [q2_dot]])
+
+        kp = 60
+        kd = 12
+
+        v = (
+            q_r_ddot.reshape(2, 1)
+            + kd * (q_r_dot.reshape(2, 1) - q_dot)
+            + kp * (q_r.reshape(2, 1) - q)
+        )
+
+        return self.model.M(x) @ v + self.model.C(x) @ q_dot
--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -5,16 +5,30 @@ class ManiuplatorModel:
     def __init__(self, Tp):
         self.Tp = Tp
         self.l1 = 0.5
-        self.r1 = 0.01
-        self.m1 = 1.
-        self.l2 = 0.5
-        self.r2 = 0.01
-        self.m2 = 1.
-        self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
-        self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
-        self.m3 = 0.0
-        self.r3 = 0.01
-        self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
+        self.r1 = 0.04
+        self.m1 = 3.0
+        self.l2 = 0.4
+        self.r2 = 0.04
+        self.m2 = 2.4
+        self.I_1 = 1 / 12 * self.m1 * (3 * self.r1**2 + self.l1**2)
+        self.I_2 = 1 / 12 * self.m2 * (3 * self.r2**2 + self.l2**2)
+        self.m3 = 1.0
+        self.r3 = 0.05
+        self.I_3 = 2.0 / 5 * self.m3 * self.r3**2
+
+        self.d1 = self.l1 / 2
+        self.d2 = self.l2 / 2
+
+        self.alpha = (
+            self.m1 * self.d1**2
+            + self.I_1
+            + self.m2 * (self.l1**2 + self.d2**2)
+            + self.I_2
+            + self.m3 * (self.l1**2 + self.l2**2)
+            + self.I_3
+        )
+        self.betha = self.m2 * self.l1 * self.d2 + self.m3 * self.l1 * self.l2
+        self.gamma = self.m2 * self.d2**2 + self.I_2 + self.m3 * self.l2**2
 
     def M(self, x):
         """
@@ -22,7 +36,16 @@ class ManiuplatorModel:
         (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        return np.array(
+            [
+                [
+                    self.alpha + 2 * self.betha * np.cos(q2),
+                    self.gamma + self.betha * np.cos(q2),
+                ],
+                [self.gamma + self.betha * np.cos(q2), self.gamma],
+            ]
+        )
 
     def C(self, x):
         """
@@ -30,4 +53,13 @@ class ManiuplatorModel:
         in the exercise (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        return NotImplementedError()
+
+        return np.array(
+            [
+                [
+                    -self.betha * np.sin(q2) * q2_dot,
+                    -self.betha * np.sin(q2) * (q1_dot + q2_dot),
+                ],
+                [self.betha * np.sin(q2) * q1_dot, 0],
+            ]
+        )
