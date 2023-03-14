eaafeb694c980f00a5cab031d65d58d14113dece
Andrzej Rafalski <anraf1001@gmail.com>
2023-03-14 23:29:31 +0100

    CHange m3 mass to nonzero value

--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -12,7 +12,7 @@ class ManiuplatorModel:
         self.m2 = 2.4
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1**2 + self.l1**2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2**2 + self.l2**2)
-        self.m3 = 0.0
+        self.m3 = 1.0
         self.r3 = 0.05
         self.I_3 = 2.0 / 5 * self.m3 * self.r3**2
 
@@ -24,9 +24,11 @@ class ManiuplatorModel:
             + self.I_1
             + self.m2 * (self.l1**2 + self.d2**2)
             + self.I_2
+            + self.m3 * (self.l1**2 + self.l2**2)
+            + self.I_3
         )
-        self.betha = self.m2 * self.l1 * self.d2
-        self.gamma = self.m2 * self.d2**2 + self.I_2
+        self.betha = self.m2 * self.l1 * self.d2 + self.m3 * self.l1 * self.l2
+        self.gamma = self.m2 * self.d2**2 + self.I_2 + self.m3 * self.l2**2
 
     def M(self, x):
         """
