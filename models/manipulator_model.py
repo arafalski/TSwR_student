388965ca411f27e18a5170500b409955f0f90c87
Andrzej Rafalski <anraf1001@gmail.com>
2023-03-14 23:23:40 +0100

    Refactor model

--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -5,16 +5,28 @@ class ManiuplatorModel:
     def __init__(self, Tp):
         self.Tp = Tp
         self.l1 = 0.5
-        self.r1 = 0.01
-        self.m1 = 1.0
-        self.l2 = 0.5
-        self.r2 = 0.01
-        self.m2 = 1.0
+        self.r1 = 0.04
+        self.m1 = 3.0
+        self.l2 = 0.4
+        self.r2 = 0.04
+        self.m2 = 2.4
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1**2 + self.l1**2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2**2 + self.l2**2)
         self.m3 = 0.0
-        self.r3 = 0.01
+        self.r3 = 0.05
         self.I_3 = 2.0 / 5 * self.m3 * self.r3**2
+
+        self.d1 = self.l1 / 2
+        self.d2 = self.l2 / 2
+
+        self.alpha = (
+            self.m1 * self.d1**2
+            + self.I_1
+            + self.m2 * (self.l1**2 + self.d2**2)
+            + self.I_2
+        )
+        self.betha = self.m2 * self.l1 * self.d2
+        self.gamma = self.m2 * self.d2**2 + self.I_2
 
     def M(self, x):
         """
@@ -23,19 +35,13 @@ class ManiuplatorModel:
         """
         q1, q2, q1_dot, q2_dot = x
 
-        d1 = self.l1 / 2
-        d2 = self.l2 / 2
-
-        alpha = (
-            self.m1 * d1**2 + self.I_1 + self.m2 * (self.l1**2 + d2**2) + self.I_2
-        )
-        betha = self.m2 * self.l1 * d2
-        gamma = self.m2 * d2**2 + self.I_2
-
         return np.array(
             [
-                [alpha + 2 * betha * np.cos(q2), gamma + betha * np.cos(q2)],
-                [gamma + betha * np.cos(q2), gamma],
+                [
+                    self.alpha + 2 * self.betha * np.cos(q2),
+                    self.gamma + self.betha * np.cos(q2),
+                ],
+                [self.gamma + self.betha * np.cos(q2), self.gamma],
             ]
         )
 
@@ -46,12 +52,12 @@ class ManiuplatorModel:
         """
         q1, q2, q1_dot, q2_dot = x
 
-        d2 = self.l2 / 2
-        betha = self.m2 * self.l1 * d2
-
         return np.array(
             [
-                [-betha * np.sin(q2) * q2_dot, -betha * np.sin(q2) * (q1_dot + q2_dot)],
-                [betha * np.sin(q2) * q1_dot, 0],
+                [
+                    -self.betha * np.sin(q2) * q2_dot,
+                    -self.betha * np.sin(q2) * (q1_dot + q2_dot),
+                ],
+                [self.betha * np.sin(q2) * q1_dot, 0],
             ]
         )
