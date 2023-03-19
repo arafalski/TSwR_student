161f2d7c810580c058efa2be9106feb5e2c1b20f
Andrzej Rafalski <anraf1001@gmail.com>
2023-03-19 19:58:29 +0100

    Implement MMA controller

--- controllers/mma_controller.py
+++ controllers/mma_controller.py
@@ -1,5 +1,6 @@
 import numpy as np
 from .controller import Controller
+from models.manipulator_model import ManiuplatorModel
 
 
 class MMAController(Controller):
@@ -8,19 +9,46 @@ class MMAController(Controller):
         # I:   m3=0.1,  r3=0.05
         # II:  m3=0.01, r3=0.01
         # III: m3=1.0,  r3=0.3
-        self.models = [None, None, None]
+        self.models = [
+            ManiuplatorModel(Tp, m3=0.1, r3=0.05),
+            ManiuplatorModel(Tp, m3=0.01, r3=0.01),
+            ManiuplatorModel(Tp, m3=1.0, r3=0.3),
+        ]
         self.i = 0
+        self.last_u = np.zeros(2)
+        self.last_x = np.zeros(4)
 
     def choose_model(self, x):
         # TODO: Implement procedure of choosing the best fitting model from self.models (by setting self.i)
-        pass
+        x_est = []
+
+        for model in self.models:
+            x_est.append(model.dx(self.last_u, self.last_x))
+
+        model_errors = [np.linalg.norm(model_x - x.reshape(4, 1)) for model_x in x_est]
+        self.i = model_errors.index(min(model_errors))
 
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         self.choose_model(x)
-        q = x[:2]
-        q_dot = x[2:]
-        v = q_r_ddot # TODO: add feedback
+
+        q1, q2, q1_dot, q2_dot = x
+
+        q = np.array([[q1], [q2]])
+        q_dot = np.array([[q1_dot], [q2_dot]])
+
+        kp = 60
+        kd = 12
+
+        v = (
+            q_r_ddot.reshape(2, 1)
+            + kd * (q_r_dot.reshape(2, 1) - q_dot)
+            + kp * (q_r.reshape(2, 1) - q)
+        )
+
         M = self.models[self.i].M(x)
         C = self.models[self.i].C(x)
-        u = M @ v[:, np.newaxis] + C @ q_dot[:, np.newaxis]
+        u = M @ v + C @ q_dot
+
+        self.last_u = u
+        self.last_x = x
         return u
