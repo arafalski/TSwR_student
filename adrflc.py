bbcfc4984808f195231864d8ccfc6d0f944623b9
Andrzej Rafalski <anraf1001@gmail.com>
2023-04-01 15:46:19 +0200

    Add ADRFLC implementation

--- adrflc.py
+++ adrflc.py
@@ -13,14 +13,17 @@ end = 5
 traj_gen = Sinusoidal(np.array([0., 1.]), np.array([2., 2.]), np.array([0., 0.]))
 # traj_gen = Poly3(np.array([0., 0.]), np.array([pi/4, pi/6]), end)
 
-b_est_1 = None
-b_est_2 = None
-kp_est_1 = None
-kp_est_2 = None
-kd_est_1 = None
-kd_est_2 = None
-p1 = None
-p2 = None
+p1 = 200
+p2 = 200
+
+omega_c_1 = 0.2 * p1
+omega_c_2 = 0.2 * p2
+ksi = 1
+
+kp_est_1 = omega_c_1**2
+kp_est_2 = omega_c_2**2
+kd_est_1 = 2 * ksi * omega_c_1
+kd_est_2 = 2 * ksi * omega_c_2
 
 q0, qdot0, _ = traj_gen.generate(0.)
 q1_0 = np.array([q0[0], qdot0[0]])
